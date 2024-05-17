56de05c82e790597a4213291f284bb6ff6c343e1
YooJaeHyeok <yoojh5685@gmail.com>
2024-05-17 19:56:33 +0900

    main+reverse

--- main.cpp
+++ main.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 #include <string>
 #include "uppercase.h"
+#include "lowercase.h"
 using namespace std;
 int main() {
     string text = "Hello, World!";
     string upperText = toUpperCase(text);
     cout << "Upper case: " << upperText << endl;
+    string lowerText = toLowerCase(text);
+    cout << "Lower case: " << lowerText << endl;
     return 0;
 }
