b56397685cefeb9181155ca7440711cc181d612b
minzzzun <22000078@handong.ac.kr>
2024-05-17 19:55:59 +0900

    toLower 함수 추가

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include <string>
 #include "uppercase.h"
+#include "lowercase.h"
 using namespace std;
 int main() {
     string text = "Hello, World!";
     string upperText = toUpperCase(text);
     cout << "Upper case: " << upperText << endl;
-    return 0;
-}
+    string lowerText = toLowerCase(text);
+    cout << "Lower case: " << lowerText << endl;
+return 0; }
